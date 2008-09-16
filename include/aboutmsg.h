21a6a62a8b46d6ce479c2ea78c7e2c198af0b812
rpolzer <rpolzer@61c419a2-8eb2-4b30-bcec-8cead039b335>
2008-09-16 17:52:41 +0000

    add version/aboutmsg replacements
    git-svn-id: svn://svn.icculus.org/netradiant/trunk@69 61c419a2-8eb2-4b30-bcec-8cead039b335

---
+++ include/aboutmsg.h
@@ -0,0 +1,4 @@
+// Makefile appends preprocessor flags instead now
+#ifndef RADIANT_ABOUTMSG
+#error no RADIANT_ABOUTMSG defined
+#endif
---
+++ include/version.h
@@ -0,0 +1,10 @@
+// Makefile appends preprocessor flags instead now
+#ifndef RADIANT_VERSION
+#error no RADIANT_VERSION defined
+#endif
+#ifndef RADIANT_MAJOR_VERSION
+#error no RADIANT_MAJOR_VERSION defined
+#endif
+#ifndef RADIANT_MINOR_VERSION
+#error no RADIANT_MINOR_VERSION defined
+#endif
