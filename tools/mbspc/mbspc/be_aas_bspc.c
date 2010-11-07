fd22d6abbfcb5b2c32fab0dd1268b032adf56b2e
Ben Noordhuis <info@bnoordhuis.nl>
2010-11-08 00:28:30 +0100

    Add #include, fixes 'implicit declaration of function' warnings.

--- tools/mbspc/mbspc/be_aas_bspc.c
+++ tools/mbspc/mbspc/be_aas_bspc.c
@@ -37,6 +37,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "../botlib/be_aas_cluster.h"
 #include "../botlib/be_aas_reach.h"
 #include "../qcommon/cm_public.h"
+#include "be_aas_bspc.h"
 
 #include <stdarg.h>
 
