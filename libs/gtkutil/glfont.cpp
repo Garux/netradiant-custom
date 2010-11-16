a9e31a3db5dbebd072ff186282ac291be43e3354
Rudolf Polzer <divVerent@xonotic.org>
2010-11-16 14:07:06 +0100

    glfont.cpp: add an internal class that uses Pango

--- libs/gtkutil/glfont.cpp
+++ libs/gtkutil/glfont.cpp
@@ -157,3 +157,142 @@ void glfont_release(GLFont& font)
   font = GLFont(0, 0, 0, 0);
 }
 #endif
+
+
+
+// new font code ripped from ZeroRadiant (not in use yet)
+
+#include <pango/pangoft2.h>
+#include "igl.h"
+
+class GLFontInternal
+{
+	const char *font_string;
+	int font_height;
+	int font_ascent;
+	int font_descent;
+	int y_offset_bitmap_render_pango_units;
+	PangoContext *ft2_context;
+
+	public:
+	GLFontInternal(const char *_font_string): font_string(font_string)
+	{
+		PangoFontDescription *font_desc;
+		PangoLayout *layout;
+		PangoRectangle log_rect;
+		int font_ascent_pango_units;
+		int font_descent_pango_units;
+
+		// This call is deprecated so we'll have to fix it sometime.
+		ft2_context = pango_ft2_get_context(72, 72);
+
+		font_desc = pango_font_description_from_string(font_string);
+		pango_context_set_font_description(ft2_context, font_desc);
+		pango_font_description_free(font_desc);
+
+		layout = pango_layout_new(ft2_context);
+#if !PANGO_VERSION_CHECK(1,22,0)
+		PangoLayoutIter *iter;  
+		iter = pango_layout_get_iter(layout);
+		font_ascent_pango_units = pango_layout_iter_get_baseline(iter);
+		pango_layout_iter_free(iter);
+#else
+		font_ascent_pango_units = pango_layout_get_baseline(layout);
+#endif
+		pango_layout_get_extents(layout, NULL, &log_rect);
+		g_object_unref(G_OBJECT(layout));
+		font_descent_pango_units = log_rect.height - font_ascent_pango_units;
+
+		font_ascent = PANGO_PIXELS_CEIL(font_ascent_pango_units);
+		font_descent = PANGO_PIXELS_CEIL(font_descent_pango_units);
+		font_height = font_ascent + font_descent;
+		y_offset_bitmap_render_pango_units = (font_ascent * PANGO_SCALE) - font_ascent_pango_units;
+	}
+
+	~GLFontInternal()
+	{
+		g_object_unref(G_OBJECT(ft2_context));
+	}
+
+	// Renders the input text at the current location with the current color.
+	// The X position of the current location is used to place the left edge of the text image,
+	// where the text image bounds are defined as the logical extents of the line of text.
+	// The Y position of the current location is used to place the bottom of the text image.
+	// You should offset the Y position by the amount returned by gtk_glwidget_font_descent()
+	// if you want to place the baseline of the text image at the current Y position.
+	// Note: A problem with this function is that if the lower left corner of the text falls
+	// just a hair outside of the viewport (meaning the current raster position is invalid),
+	// then no text will be rendered.  The solution to this is a very hacky one.  You can search
+	// Google for "glDrawPixels clipping".
+	void printString(const char *s)
+	{
+		// The idea for this code initially came from the font-pangoft2.c example that comes with GtkGLExt.
+
+		PangoLayout *layout;
+		PangoRectangle log_rect;
+		FT_Bitmap bitmap;
+		unsigned char *begin_bitmap_buffer;
+		GLfloat color[4];
+		GLint previous_unpack_alignment;
+		GLboolean previous_blend_enabled;
+		GLint previous_blend_func_src;
+		GLint previous_blend_func_dst;
+		GLfloat previous_red_bias;
+		GLfloat previous_green_bias;
+		GLfloat previous_blue_bias;
+		GLfloat previous_alpha_scale;
+
+		layout = pango_layout_new(ft2_context);
+		pango_layout_set_width(layout, -1); // -1 no wrapping.  All text on one line.
+		pango_layout_set_text(layout, s, -1); // -1 null-terminated string.
+		pango_layout_get_extents(layout, NULL, &log_rect);
+
+		if (log_rect.width > 0 && log_rect.height > 0) {
+			bitmap.rows = font_ascent + font_descent;
+			bitmap.width = PANGO_PIXELS_CEIL(log_rect.width);
+			bitmap.pitch = -bitmap.width; // Rendering it "upside down" for OpenGL.
+			begin_bitmap_buffer = (unsigned char *) g_malloc(bitmap.rows * bitmap.width);
+			memset(begin_bitmap_buffer, 0, bitmap.rows * bitmap.width);
+			bitmap.buffer = begin_bitmap_buffer + (bitmap.rows - 1) * bitmap.width; // See pitch above.
+			bitmap.num_grays = 0xff;
+			bitmap.pixel_mode = FT_PIXEL_MODE_GRAY;
+			pango_ft2_render_layout_subpixel(&bitmap, layout, -log_rect.x,
+					y_offset_bitmap_render_pango_units);
+			GlobalOpenGL().m_glGetFloatv(GL_CURRENT_COLOR, color);
+
+			// Save state.  I didn't see any OpenGL push/pop operations for these.
+			// Question: Is saving/restoring this state necessary?  Being safe.
+			GlobalOpenGL().m_glGetIntegerv(GL_UNPACK_ALIGNMENT, &previous_unpack_alignment);
+			previous_blend_enabled = GlobalOpenGL().m_glIsEnabled(GL_BLEND);
+			GlobalOpenGL().m_glGetIntegerv(GL_BLEND_SRC, &previous_blend_func_src);
+			GlobalOpenGL().m_glGetIntegerv(GL_BLEND_DST, &previous_blend_func_dst);
+			GlobalOpenGL().m_glGetFloatv(GL_RED_BIAS, &previous_red_bias);
+			GlobalOpenGL().m_glGetFloatv(GL_GREEN_BIAS, &previous_green_bias);
+			GlobalOpenGL().m_glGetFloatv(GL_BLUE_BIAS, &previous_blue_bias);
+			GlobalOpenGL().m_glGetFloatv(GL_ALPHA_SCALE, &previous_alpha_scale);
+
+			GlobalOpenGL().m_glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+			GlobalOpenGL().m_glEnable(GL_BLEND);
+			GlobalOpenGL().m_glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			GlobalOpenGL().m_glPixelTransferf(GL_RED_BIAS, color[0]);
+			GlobalOpenGL().m_glPixelTransferf(GL_GREEN_BIAS, color[1]);
+			GlobalOpenGL().m_glPixelTransferf(GL_BLUE_BIAS, color[2]);
+			GlobalOpenGL().m_glPixelTransferf(GL_ALPHA_SCALE, color[3]);
+
+			GlobalOpenGL().m_glDrawPixels(bitmap.width, bitmap.rows,
+					GL_ALPHA, GL_UNSIGNED_BYTE, begin_bitmap_buffer);
+			g_free(begin_bitmap_buffer);
+
+			// Restore state in reverse order of how we set it.
+			GlobalOpenGL().m_glPixelTransferf(GL_ALPHA_SCALE, previous_alpha_scale);
+			GlobalOpenGL().m_glPixelTransferf(GL_BLUE_BIAS, previous_blue_bias);
+			GlobalOpenGL().m_glPixelTransferf(GL_GREEN_BIAS, previous_green_bias);
+			GlobalOpenGL().m_glPixelTransferf(GL_RED_BIAS, previous_red_bias);
+			GlobalOpenGL().m_glBlendFunc(previous_blend_func_src, previous_blend_func_dst);
+			if (!previous_blend_enabled) { GlobalOpenGL().m_glDisable(GL_BLEND); }
+			GlobalOpenGL().m_glPixelStorei(GL_UNPACK_ALIGNMENT, previous_unpack_alignment);
+		}
+
+		g_object_unref(G_OBJECT(layout));
+	}
+};
