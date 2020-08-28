bd8a597016c557284839c445351c4cf554239eb5
Garux <garux@mail.ru>
2020-08-28 19:55:35 +0300

    enable console logging by default to simplify troubleshooting it is enabled anyway in Sys_Print() if ( level == SYS_ERR ), but later

--- radiant/console.cpp
+++ radiant/console.cpp
@@ -43,7 +43,7 @@ namespace
 FILE* g_hLogFile;
 }
 
-bool g_Console_enableLogging = false;
+bool g_Console_enableLogging = true;
 
 // called whenever we need to open/close/check the console log file
 void Sys_LogFile( bool enable ){
