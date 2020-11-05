a28add796c99f5a2328ff2148bcc144c39ec800a
Garux <garux@mail.ru>
2020-11-05 14:24:45 +0300

    fix incompatible function type

--- plugins/imagepng/plugin.cpp
+++ plugins/imagepng/plugin.cpp
@@ -42,7 +42,7 @@ void user_error_fn( png_structp png_ptr, png_const_charp error_msg ){
 	longjmp( png_jmpbuf(png_ptr), 0 );
 }
 
-void user_read_data( png_structp png_ptr, png_bytep data, png_uint_32 length ){
+void user_read_data( png_structp png_ptr, png_bytep data, size_t length ){
 	png_bytep *p_p_fbuffer = (png_bytep*)png_get_io_ptr( png_ptr );
 	memcpy( data, *p_p_fbuffer, length );
 	*p_p_fbuffer += length;
