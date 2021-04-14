89110afef6b101c24dd976ee25d62a6405093318
Garux <garux@mail.ru>
2021-04-14 16:44:01 +0300

    fix default model box

--- plugins/md3model/model.h
+++ plugins/md3model/model.h
@@ -546,12 +546,12 @@ inline void Model_constructNull( Model& model ){
 	surface.indices().reserve( 36 );
 
 	RenderIndex indices[36] = {
-		0,  1,  2,  0,  2,  3,
-		4,  5,  6,  4,  6,  7,
-		8,  9, 10,  8, 10, 11,
+		 0,  1,  2,  0,  2,  3,
+		 4,  5,  6,  4,  6,  7,
+		 8,  9, 10,  8, 10, 11,
 		12, 13, 14, 12, 14, 15,
 		16, 17, 18, 16, 18, 19,
-		20, 21, 22, 10, 22, 23,
+		20, 21, 22, 20, 22, 23,
 	};
 
 	for ( RenderIndex* i = indices; i != indices + ( sizeof( indices ) / sizeof( RenderIndex ) ); ++i )
