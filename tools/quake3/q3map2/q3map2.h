Garux/netradiant-custom

commit 977781a62106fa5bc860caeccebaafb826e48f38

minor tweaks

tools/quake3/q3map2/q3map2.h
@@ -1741,7 +1741,6 @@ void                        MakeEntityDecals( entity_t *e );
 std::array<Vector3, 2>      TextureAxisFromPlane( const plane_t& plane );
 
 /* vis.c */
-fixedWinding_t              *NewFixedWinding( int points );
 int                         VisMain( Args& args );
 
 /* visflow.c */

tools/quake3/q3map2/shaders.cpp
@@ -40,13 +40,6 @@
  */
 
 void ColorMod( colorMod_t *cm, int numVerts, bspDrawVert_t *drawVerts ){
-	int i, j;
-	float c;
-	Color4f mult, add;
-	bspDrawVert_t   *dv;
-	colorMod_t      *cm2;
-
-
 	/* dummy check */
 	if ( cm == NULL || numVerts < 1 || drawVerts == NULL ) {
 		return;
@@ -54,83 +47,81 @@ void ColorMod( colorMod_t *cm, int numVerts, bspDrawVert_t *drawVerts ){
 
 
 	/* walk vertex list */
-	for ( i = 0; i < numVerts; i++ )
+	for ( bspDrawVert_t& dv : Span( drawVerts, numVerts ) )
 	{
-		/* get vertex */
-		dv = &drawVerts[ i ];
-
 		/* walk colorMod list */
-		for ( cm2 = cm; cm2 != NULL; cm2 = cm2->next )
+		for ( ; cm != NULL; cm = cm->next )
 		{
+			float c;
 			/* default */
-			mult.set( 1 );
-			add.set( 0 );
+			Color4f mult( 1, 1, 1, 1 );
+			Color4f add( 0, 0, 0, 0 );
 
-			const Vector3 cm2_vec3 = vector3_from_array( cm2->data );
+			const Vector3 cm_vec3 = vector3_from_array( cm->data );
 			/* switch on type */
-			switch ( cm2->type )
+			switch ( cm->type )
 			{
 			case EColorMod::ColorSet:
 				mult.rgb().set( 0 );
-				add.rgb() = cm2_vec3 * 255.0f;
+				add.rgb() = cm_vec3 * 255.0f;
 				break;
 
 			case EColorMod::AlphaSet:
 				mult.alpha() = 0.0f;
-				add.alpha() = cm2->data[ 0 ] * 255.0f;
+				add.alpha() = cm->data[ 0 ] * 255.0f;
 				break;
 
 			case EColorMod::ColorScale:
-				mult.rgb() = cm2_vec3;
+				mult.rgb() = cm_vec3;
 				break;
 
 			case EColorMod::AlphaScale:
-				mult.alpha() = cm2->data[ 0 ];
+				mult.alpha() = cm->data[ 0 ];
 				break;
 
 			case EColorMod::ColorDotProduct:
-				c = vector3_dot( dv->normal, cm2_vec3 );
+				c = vector3_dot( dv.normal, cm_vec3 );
 				mult.rgb().set( c );
 				break;
 
 			case EColorMod::ColorDotProductScale:
-				c = vector3_dot( dv->normal, cm2_vec3 );
-				c = ( c - cm2->data[3] ) / ( cm2->data[4] - cm2->data[3] );
+				c = vector3_dot( dv.normal, cm_vec3 );
+				c = ( c - cm->data[3] ) / ( cm->data[4] - cm->data[3] );
 				mult.rgb().set( c );
 				break;
 
 			case EColorMod::AlphaDotProduct:
-				mult.alpha() = vector3_dot( dv->normal, cm2_vec3 );
+				mult.alpha() = vector3_dot( dv.normal, cm_vec3 );
 				break;
 
 			case EColorMod::AlphaDotProductScale:
-				c = vector3_dot( dv->normal, cm2_vec3 );
-				c = ( c - cm2->data[3] ) / ( cm2->data[4] - cm2->data[3] );
+				c = vector3_dot( dv.normal, cm_vec3 );
+				c = ( c - cm->data[3] ) / ( cm->data[4] - cm->data[3] );
 				mult.alpha() = c;
 				break;
 
 			case EColorMod::ColorDotProduct2:
-				c = vector3_dot( dv->normal, cm2_vec3 );
+				c = vector3_dot( dv.normal, cm_vec3 );
 				c *= c;
 				mult.rgb().set( c );
 				break;
 
 			case EColorMod::ColorDotProduct2Scale:
-				c = vector3_dot( dv->normal, cm2_vec3 );
+				c = vector3_dot( dv.normal, cm_vec3 );
 				c *= c;
-				c = ( c - cm2->data[3] ) / ( cm2->data[4] - cm2->data[3] );
+				c = ( c - cm->data[3] ) / ( cm->data[4] - cm->data[3] );
 				mult.rgb().set( c );
 				break;
 
 			case EColorMod::AlphaDotProduct2:
-				mult.alpha() = vector3_dot( dv->normal, cm2_vec3 );
+				mult.alpha() = vector3_dot( dv.normal, cm_vec3 );
 				mult.alpha() *= mult.alpha();
 				break;
 
 			case EColorMod::AlphaDotProduct2Scale:
-				c = vector3_dot( dv->normal, cm2_vec3 );
+				c = vector3_dot( dv.normal, cm_vec3 );
 				c *= c;
-				c = ( c - cm2->data[3] ) / ( cm2->data[4] - cm2->data[3] );
+				c = ( c - cm->data[3] ) / ( cm->data[4] - cm->data[3] );
 				mult.alpha() = c;
 				break;
 
@@ -139,9 +130,9 @@ void ColorMod( colorMod_t *cm, int numVerts, bspDrawVert_t *drawVerts ){
 			}
 
 			/* apply mod */
-			for ( j = 0; j < MAX_LIGHTMAPS; j++ )
+			for ( auto& color : dv.color )
 			{
-				dv->color[ j ] = color_to_byte( mult * static_cast<BasicVector4<byte>>( dv->color[ j ] ) + add );
+				color = color_to_byte( mult * static_cast<BasicVector4<byte>>( color ) + add );
 			}
 		}
 	}
@@ -253,11 +244,8 @@ bool ApplySurfaceParm( const char *name, int *contentFlags, int *surfaceFlags, i
 	}
 
 	/* check custom info parms */
-	for ( int i = 0; i < numCustSurfaceParms; i++ )
+	for ( const surfaceParm_t& sp : Span( custSurfaceParms, numCustSurfaceParms ) )
 	{
-		/* get surfaceparm */
-		const surfaceParm_t& sp = custSurfaceParms[ i ];
-
 		/* match? */
 		if ( striEqual( name, sp.name ) ) {
 			/* clear and set flags */
@@ -314,33 +302,21 @@ void BeginMapShaderFile( const char *mapFile ){
  */
 
 void WriteMapShaderFile( void ){
-	FILE            *file;
-	shaderInfo_t    *si;
-	int i, num;
-
-
 	/* dummy check */
 	if ( mapShaderFile.empty() ) {
 		return;
 	}
 
 	/* are there any custom shaders? */
-	for ( i = 0, num = 0; i < numShaderInfo; i++ )
-	{
-		if ( shaderInfo[ i ].custom ) {
-			break;
-		}
-	}
-	if ( i == numShaderInfo ) {
+	if( std::none_of( shaderInfo, shaderInfo + numShaderInfo, []( const shaderInfo_t& si ){ return si.custom; } ) )
 		return;
-	}
 
 	/* note it */
 	Sys_FPrintf( SYS_VRB, "--- WriteMapShaderFile ---\n" );
 	Sys_FPrintf( SYS_VRB, "Writing %s", mapShaderFile.c_str() );
 
 	/* open shader file */
-	file = fopen( mapShaderFile.c_str(), "wt" );
+	FILE *file = fopen( mapShaderFile.c_str(), "wt" );
 	if ( file == NULL ) {
 		Sys_Warning( "Unable to open map shader file %s for writing\n", mapShaderFile.c_str() );
 		return;
@@ -354,20 +330,18 @@ void WriteMapShaderFile( void ){
 	         mapName.c_str() );
 
 	/* walk the shader list */
-	for ( i = 0, num = 0; i < numShaderInfo; i++ )
+	int num = 0;
+	for ( const shaderInfo_t& si : Span( shaderInfo, numShaderInfo ) )
 	{
-		/* get the shader and print it */
-		si = &shaderInfo[ i ];
-		if ( !si->custom || strEmptyOrNull( si->shaderText ) ) {
-			continue;
+		if ( si.custom && !strEmptyOrNull( si.shaderText ) ) {
+			num++;
+
+			/* print it to the file */
+			fprintf( file, "%s%s\n", si.shader.c_str(), si.shaderText );
+			//Sys_Printf( "%s%s\n", si.shader.c_str(), si.shaderText ); /* FIXME: remove debugging code */
+
+			Sys_FPrintf( SYS_VRB, "." );
 		}
-		num++;
-
-		/* print it to the file */
-		fprintf( file, "%s%s\n", si->shader.c_str(), si->shaderText );
-		//Sys_Printf( "%s%s\n", si->shader, si->shaderText ); /* FIXME: remove debugging code */
-
-		Sys_FPrintf( SYS_VRB, "." );
 	}
 
 	/* close the shader */

tools/quake3/common/inout.cpp
@@ -194,7 +194,6 @@ void xml_Winding( const char *msg, const Vector3 p[], int numpoints, bool die ){
 	char buf[WINDING_BUFSIZE];
 	char smlbuf[128];
 	char level[2];
-	int i;
 
 	node = xmlNewNode( NULL, (const xmlChar*)"windingmsg" );
 	xmlNodeAddContent( node, (const xmlChar*)msg );
@@ -203,11 +202,11 @@ void xml_Winding( const char *msg, const Vector3 p[], int numpoints, bool die ){
 	xmlSetProp( node, (const xmlChar*)"level", (const xmlChar *)level );
 	// a 'winding' node
 	sprintf( buf, "%i ", numpoints );
-	for ( i = 0; i < numpoints; i++ )
+	for ( int i = 0; i < numpoints; ++i )
 	{
 		sprintf( smlbuf, "(%g %g %g)", p[i][0], p[i][1], p[i][2] );
 		// don't overflow
-		if ( strlen( buf ) + strlen( smlbuf ) > WINDING_BUFSIZE ) {
+		if ( strlen( buf ) + strlen( smlbuf ) >= WINDING_BUFSIZE ) {
 			break;
 		}
 		strcat( buf, smlbuf );

tools/quake3/common/scriplib.cpp
@@ -315,11 +315,9 @@ void MatchToken( const char *match ) {
 
 template<typename T>
 void Parse1DMatrix( int x, T *m ) {
-	int i;
-
 	MatchToken( "(" );
 
-	for ( i = 0 ; i < x ; i++ ) {
+	for ( int i = 0; i < x; ++i ) {
 		GetToken( false );
 		m[i] = atof( token );
 	}
@@ -330,11 +328,9 @@ template void Parse1DMatrix<float>( int x, float *m );
 template void Parse1DMatrix<double>( int x, double *m );
 
 void Parse2DMatrix( int y, int x, float *m ) {
-	int i;
-
 	MatchToken( "(" );
 
-	for ( i = 0 ; i < y ; i++ ) {
+	for ( int i = 0; i < y; ++i ) {
 		Parse1DMatrix( x, m + i * x );
 	}
 
@@ -342,11 +338,9 @@ void Parse2DMatrix( int y, int x, float *m ) {
 }
 
 void Parse3DMatrix( int z, int y, int x, float *m ) {
-	int i;
-
 	MatchToken( "(" );
 
-	for ( i = 0 ; i < z ; i++ ) {
+	for ( int i = 0; i < z; ++i ) {
 		Parse2DMatrix( y, x, m + i * x * y );
 	}
 
@@ -355,10 +349,8 @@ void Parse3DMatrix( int z, int y, int x, float *m ) {
 
 
 void Write1DMatrix( FILE *f, int x, float *m ) {
-	int i;
-
 	fprintf( f, "( " );
-	for ( i = 0 ; i < x ; i++ ) {
+	for ( int i = 0; i < x; ++i ) {
 		if ( m[i] == (int)m[i] ) {
 			fprintf( f, "%i ", (int)m[i] );
 		}
@@ -370,10 +362,8 @@ void Write1DMatrix( FILE *f, int x, float *m ) {
 }
 
 void Write2DMatrix( FILE *f, int y, int x, float *m ) {
-	int i;
-
 	fprintf( f, "( " );
-	for ( i = 0 ; i < y ; i++ ) {
+	for ( int i = 0; i < y; ++i ) {
 		Write1DMatrix( f, x, m + i * x );
 		fprintf( f, " " );
 	}
@@ -382,10 +372,8 @@ void Write2DMatrix( FILE *f, int y, int x, float *m ) {
 
 
 void Write3DMatrix( FILE *f, int z, int y, int x, float *m ) {
-	int i;
-
 	fprintf( f, "(\n" );
-	for ( i = 0 ; i < z ; i++ ) {
+	for ( int i = 0; i < z; ++i ) {
 		Write2DMatrix( f, y, x, m + i * ( x * y ) );
 	}
 	fprintf( f, ")\n" );

tools/quake3/common/stringfixedsize.h
@@ -42,8 +42,8 @@ public:
 	}
 	std::size_t write( const char* buffer, std::size_t length ) override {
 		if( m_length + length < SIZE ){
-			for( auto i = length; i != 0; --i )
-				m_string[m_length++] = *buffer++;
+			std::copy_n( buffer, length, m_string + m_length );
+			m_length += length;
 			strClear( &m_string[m_length] );
 		}
 		else{

tools/quake3/q3map2/map.cpp
@@ -300,10 +300,10 @@ void SnapPlaneImproved( Plane3f& plane, int numPoints, const Vector3 *points ){
 	if ( SnapNormal( plane.normal() ) ) {
 		if ( numPoints > 0 ) {
 			// Adjust the dist so that the provided points don't drift away.
-			Vector3 center( 0 );
-			for ( int i = 0; i < numPoints; i++ )
+			DoubleVector3 center( 0 );
+			for ( const Vector3& point : Span( points, numPoints ) )
 			{
-				center += points[i];
+				center += point;
 			}
 			center /= numPoints;
 			plane.dist() = vector3_dot( plane.normal(), center );
@@ -1281,8 +1281,8 @@ void AdjustBrushesForOrigin( entity_t *ent ){
 	/* walk patch list */
 	for ( parseMesh_t *p = ent->patches; p != NULL; p = p->next )
 	{
-		for ( int i = 0; i < ( p->mesh.width * p->mesh.height ); i++ )
-			p->mesh.verts[ i ].xyz -= ent->originbrush_origin;
+		for ( bspDrawVert_t& vert : Span( p->mesh.verts, p->mesh.width * p->mesh.height ) )
+			vert.xyz -= ent->originbrush_origin;
 	}
 }
 
@@ -1330,11 +1330,9 @@ void SetEntityBounds( entity_t *e ){
  */
 
 void LoadEntityIndexMap( entity_t *e ){
-	int i, size, numLayers, w, h;
+	int numLayers, w, h;
 	const char      *indexMapFilename, *shader;
 	byte            *pixels;
-	unsigned int    *pixels32;
-	indexMap_t      *im;
 
 
 	/* this only works with bmodel ents */
@@ -1371,12 +1369,13 @@ void LoadEntityIndexMap( entity_t *e ){
 	/* handle tga image */
 	if ( path_extension_is( indexMapFilename, "tga" ) ) {
 		/* load it */
+		unsigned int    *pixels32;
 		Load32BitImage( indexMapFilename, &pixels32, &w, &h );
 
 		/* convert to bytes */
-		size = w * h;
+		const int size = w * h;
 		pixels = safe_malloc( size );
-		for ( i = 0; i < size; i++ )
+		for ( int i = 0; i < size; i++ )
 		{
 			pixels[ i ] = ( ( pixels32[ i ] & 0xFF ) * numLayers ) / 256;
 			if ( pixels[ i ] >= numLayers ) {
@@ -1396,8 +1395,8 @@ void LoadEntityIndexMap( entity_t *e ){
 		//%	Sys_Printf( "-------------------------------" );
 
 		/* fix up out-of-range values */
-		size = w * h;
-		for ( i = 0; i < size; i++ )
+		const int size = w * h;
+		for ( int i = 0; i < size; i++ )
 		{
 			if ( pixels[ i ] >= numLayers ) {
 				pixels[ i ] = numLayers - 1;
@@ -1422,7 +1421,7 @@ void LoadEntityIndexMap( entity_t *e ){
 	}
 
 	/* create a new index map */
-	im = safe_malloc( sizeof( *im ) );
+	indexMap_t *im = safe_malloc( sizeof( *im ) );
 	new ( im ) indexMap_t{}; // placement new
 
 	/* set it up */
@@ -1437,7 +1436,7 @@ void LoadEntityIndexMap( entity_t *e ){
 	if( mapEnt->read_keyvalue( offset, "_offsets", "offsets" ) ){
 		/* value is a space-separated set of numbers */
 		/* get each value */
-		for ( i = 0; i < 256 && !strEmpty( offset ); i++ )
+		for ( int i = 0; i < 256 && !strEmpty( offset ); ++i )
 		{
 			const char *space = strchr( offset, ' ' );
 			if ( space == NULL ) {

tools/quake3/q3map2/mesh.cpp
@@ -94,10 +94,8 @@ void FreeMesh( mesh_t *m ) {
 }
 
 void PrintMesh( mesh_t *m ) {
-	int i, j;
-
-	for ( i = 0 ; i < m->height ; i++ ) {
-		for ( j = 0 ; j < m->width ; j++ ) {
+	for ( int i = 0; i < m->height; ++i ) {
+		for ( int j = 0; j < m->width; ++j ) {
 			Sys_Printf( "(%5.2f %5.2f %5.2f) "
 			            , m->verts[i * m->width + j].xyz[0]
 			            , m->verts[i * m->width + j].xyz[1]
@@ -109,14 +107,11 @@ void PrintMesh( mesh_t *m ) {
 
 
 mesh_t *CopyMesh( mesh_t *mesh ) {
-	mesh_t  *out;
-	int size;
-
-	out = safe_malloc( sizeof( *out ) );
+	mesh_t *out = safe_malloc( sizeof( *out ) );
 	out->width = mesh->width;
 	out->height = mesh->height;
 
-	size = out->width * out->height * sizeof( *out->verts );
+	const int size = out->width * out->height * sizeof( *out->verts );
 	out->verts = safe_malloc( size );
 	memcpy( out->verts, mesh->verts, size );
 
@@ -130,16 +125,13 @@ mesh_t *CopyMesh( mesh_t *mesh ) {
  */
 
 mesh_t *TransposeMesh( mesh_t *in ) {
-	int w, h;
-	mesh_t      *out;
-
-	out = safe_malloc( sizeof( *out ) );
+	mesh_t *out = safe_malloc( sizeof( *out ) );
 	out->width = in->height;
 	out->height = in->width;
 	out->verts = safe_malloc( out->width * out->height * sizeof( bspDrawVert_t ) );
 
-	for ( h = 0 ; h < in->height ; h++ ) {
-		for ( w = 0 ; w < in->width ; w++ ) {
+	for ( int h = 0; h < in->height; ++h ) {
+		for ( int w = 0; w < in->width; ++w ) {
 			out->verts[ w * in->height + h ] = in->verts[ h * in->width + w ];
 		}
 	}
@@ -150,14 +142,12 @@ mesh_t *TransposeMesh( mesh_t *in ) {
 }
 
 void InvertMesh( mesh_t *in ) {
-	int w, h;
-	bspDrawVert_t temp;
-
-	for ( h = 0 ; h < in->height ; h++ ) {
-		for ( w = 0 ; w < in->width / 2 ; w++ ) {
-			temp = in->verts[ h * in->width + w ];
-			in->verts[ h * in->width + w ] = in->verts[ h * in->width + in->width - 1 - w ];
-			in->verts[ h * in->width + in->width - 1 - w ] = temp;
+	for ( int h = 0; h < in->height; ++h ) {
+		for ( int w = 0; w < in->width / 2; ++w ) {
+			std::swap(
+				in->verts[ h * in->width + w ],
+				in->verts[ h * in->width + in->width - 1 - w ]
+			);
 		}
 	}
 }
@@ -275,12 +265,9 @@ void MakeMeshNormals( mesh_t in ){
  */
 
 void PutMeshOnCurve( mesh_t in ) {
-	int i, j, m;
-
-
 	// put all the aproximating points on the curve
-	for ( i = 0 ; i < in.width ; i++ ) {
-		for ( j = 1 ; j < in.height ; j += 2 ) {
+	for ( int i = 0; i < in.width; ++i ) {
+		for ( int j = 1; j < in.height; j += 2 ) {
 			const int idx = j * in.width + i;
 			const int idprev = ( j + 1 ) * in.width + i;
 			const int idnext = ( j - 1 ) * in.width + i;
@@ -289,7 +276,7 @@ void PutMeshOnCurve( mesh_t in ) {
 			/* ydnar: interpolating st coords */
 			in.verts[idx].st = vector2_mid( vector2_mid( in.verts[idx].st, in.verts[idprev].st ),
 			                                vector2_mid( in.verts[idx].st, in.verts[idnext].st ) );
-			for ( m = 0; m < MAX_LIGHTMAPS; m++ )
+			for ( int m = 0; m < MAX_LIGHTMAPS; ++m )
 			{
 				in.verts[idx].lightmap[ m ] = vector2_mid( vector2_mid( in.verts[idx].lightmap[ m ], in.verts[idprev].lightmap[ m ] ),
 				                                           vector2_mid( in.verts[idx].lightmap[ m ], in.verts[idnext].lightmap[ m ] ) );
@@ -297,15 +284,15 @@ void PutMeshOnCurve( mesh_t in ) {
 		}
 	}
 
-	for ( j = 0 ; j < in.height ; j++ ) {
-		for ( i = 1 ; i < in.width ; i += 2 ) {
+	for ( int j = 0; j < in.height; ++j ) {
+		for ( int i = 1; i < in.width; i += 2 ) {
 			const int idx = j * in.width + i;
 			in.verts[idx].xyz = vector3_mid( vector3_mid( in.verts[idx].xyz, in.verts[idx + 1].xyz ),
 			                                 vector3_mid( in.verts[idx].xyz, in.verts[idx - 1].xyz ) );
 			/* ydnar: interpolating st coords */
 			in.verts[idx].st = vector2_mid( vector2_mid( in.verts[idx].st, in.verts[idx + 1].st ),
 			                                vector2_mid( in.verts[idx].st, in.verts[idx - 1].st ) );
-			for ( m = 0; m < MAX_LIGHTMAPS; m++ )
+			for ( int m = 0; m < MAX_LIGHTMAPS; ++m )
 			{
 				in.verts[idx].lightmap[ m ] = vector2_mid( vector2_mid( in.verts[idx].lightmap[ m ], in.verts[idx + 1].lightmap[ m ] ),
 				                                           vector2_mid( in.verts[idx].lightmap[ m ], in.verts[idx - 1].lightmap[ m ] ) );
@@ -538,7 +525,7 @@ mesh_t *SubdivideMesh2( mesh_t in, int iterations ){
 				LerpDrawVert( &expand[ j + 1 ][ i ], &expand[ j + 2 ][ i ], &next );
 				LerpDrawVert( &prev, &next, &mid );
 
-				for ( k = out.height - 1; k > j  +  3; k-- )
+				for ( k = out.height - 1; k > j + 3; k-- )
 					expand[ k ][ i ] = expand[ k - 2 ][ i ];
 				expand[ j + 1 ][ i ] = prev;
 				expand[ j + 2 ][ i ] = mid;

tools/quake3/q3map2/minimap.cpp
@@ -51,13 +51,11 @@ struct minimap_t
 static minimap_t minimap;
 
 bool BrushIntersectionWithLine( const bspBrush_t& brush, const Vector3& start, const Vector3& dir, float *t_in, float *t_out ){
-	int i;
 	bool in = false, out = false;
-	bspBrushSide_t *sides = &bspBrushSides[brush.firstSide];
 
-	for ( i = 0; i < brush.numSides; ++i )
+	for ( const bspBrushSide_t& side : Span( &bspBrushSides[brush.firstSide], brush.numSides ) )
 	{
-		const bspPlane_t& p = bspPlanes[sides[i].planeNum];
+		const bspPlane_t& p = bspPlanes[side.planeNum];
 		float sn = vector3_dot( start, p.normal() );
 		float dn = vector3_dot( dir, p.normal() );
 		if ( dn == 0 ) {

commit 150c3027b0fb12bd430f7c1412972f3379586a06

fix colormod broken in 977781a62106fa5bc860caeccebaafb826e48f38

tools/quake3/q3map2/q3map2.h
@@ -1647,7 +1647,7 @@ const image_t               *ImageLoad( const char *name );
 
 
 /* shaders.c */
-void                        ColorMod( const colorMod_t *am, int numVerts, bspDrawVert_t *drawVerts );
+void                        ColorMod( const colorMod_t *colormod, int numVerts, bspDrawVert_t *drawVerts );
 
 void                        TCMod( const tcMod_t& mod, Vector2& st );
 

tools/quake3/q3map2/shaders.cpp
@@ -45,9 +45,9 @@ static int numCustSurfaceParms;
    routines for dealing with vertex color/alpha modification
  */
 
-void ColorMod( const colorMod_t *cm, int numVerts, bspDrawVert_t *drawVerts ){
+void ColorMod( const colorMod_t *colormod, int numVerts, bspDrawVert_t *drawVerts ){
 	/* dummy check */
-	if ( cm == NULL || numVerts < 1 || drawVerts == NULL ) {
+	if ( colormod == NULL || numVerts < 1 || drawVerts == NULL ) {
 		return;
 	}
 
@@ -56,7 +56,7 @@ void ColorMod( const colorMod_t *cm, int numVerts, bspDrawVert_t *drawVerts ){
 	for ( bspDrawVert_t& dv : Span( drawVerts, numVerts ) )
 	{
 		/* walk colorMod list */
-		for ( ; cm != NULL; cm = cm->next )
+		for ( const colorMod_t *cm = colormod; cm != NULL; cm = cm->next )
 		{
 			float c;
 			/* default */