DaemonEngine/daemonmap

Callback: remove fixed-arity wrappers

libs/gtkutil/accelerator.h
@@ -80,9 +80,9 @@ TextOutputStreamType& ostream_write( TextOutputStreamType& ostream, const Accele
 	return ostream;
 }
 
-void keydown_accelerators_add( Accelerator accelerator, const Callback& callback );
+void keydown_accelerators_add( Accelerator accelerator, const Callback<void()>& callback );
 void keydown_accelerators_remove( Accelerator accelerator );
-void keyup_accelerators_add( Accelerator accelerator, const Callback& callback );
+void keyup_accelerators_add( Accelerator accelerator, const Callback<void()>& callback );
 void keyup_accelerators_remove( Accelerator accelerator );
 
 void global_accel_connect_window( ui::Window window );
@@ -94,16 +94,16 @@ extern ui::AccelGroup global_accel;
 
 GClosure* global_accel_group_find( Accelerator accelerator );
 
-void global_accel_group_connect( const Accelerator& accelerator, const Callback& callback );
-void global_accel_group_disconnect( const Accelerator& accelerator, const Callback& callback );
+void global_accel_group_connect( const Accelerator& accelerator, const Callback<void()>& callback );
+void global_accel_group_disconnect( const Accelerator& accelerator, const Callback<void()>& callback );
 
 
 class Command
 {
 public:
-Callback m_callback;
+Callback<void()> m_callback;
 const Accelerator& m_accelerator;
-Command( const Callback& callback, const Accelerator& accelerator ) : m_callback( callback ), m_accelerator( accelerator ){
+Command( const Callback<void()>& callback, const Accelerator& accelerator ) : m_callback( callback ), m_accelerator( accelerator ){
 }
 };
 
@@ -112,7 +112,7 @@ class Toggle
 public:
 Command m_command;
 BoolExportCallback m_exportCallback;
-Toggle( const Callback& callback, const Accelerator& accelerator, const BoolExportCallback& exportCallback ) : m_command( callback, accelerator ), m_exportCallback( exportCallback ){
+Toggle( const Callback<void()>& callback, const Accelerator& accelerator, const BoolExportCallback& exportCallback ) : m_command( callback, accelerator ), m_exportCallback( exportCallback ){
 }
 };
 
@@ -120,9 +120,9 @@ class KeyEvent
 {
 public:
 const Accelerator& m_accelerator;
-Callback m_keyDown;
-Callback m_keyUp;
-KeyEvent( const Accelerator& accelerator, const Callback& keyDown, const Callback& keyUp ) : m_accelerator( accelerator ), m_keyDown( keyDown ), m_keyUp( keyUp ){
+Callback<void()> m_keyDown;
+Callback<void()> m_keyUp;
+KeyEvent( const Accelerator& accelerator, const Callback<void()>& keyDown, const Callback<void()>& keyUp ) : m_accelerator( accelerator ), m_keyDown( keyDown ), m_keyUp( keyUp ){
 }
 };
 

radiant/brushmodule.cpp
@@ -45,12 +45,12 @@ bool getTextureLockEnabled(){
 void Face_importSnapPlanes( bool value ){
 	Face::m_quantise = value ? quantiseInteger : quantiseFloating;
 }
-typedef FreeCaller1<bool, Face_importSnapPlanes> FaceImportSnapPlanesCaller;
+typedef FreeCaller<void(bool), Face_importSnapPlanes> FaceImportSnapPlanesCaller;
 
 void Face_exportSnapPlanes( const BoolImportCallback& importer ){
 	importer( Face::m_quantise == quantiseInteger );
 }
-typedef FreeCaller1<const BoolImportCallback&, Face_exportSnapPlanes> FaceExportSnapPlanesCaller;
+typedef FreeCaller<void(const BoolImportCallback&), Face_exportSnapPlanes> FaceExportSnapPlanesCaller;
 
 void Brush_constructPreferences( PreferencesPage& page ){
 	page.appendCheckBox(
@@ -80,7 +80,7 @@ void Brush_constructPage( PreferenceGroup& group ){
 	Brush_constructPreferences( page );
 }
 void Brush_registerPreferencesPage(){
-	PreferencesDialog_addSettingsPage( FreeCaller1<PreferenceGroup&, Brush_constructPage>() );
+	PreferencesDialog_addSettingsPage( FreeCaller<void(PreferenceGroup&), Brush_constructPage>() );
 }
 
 void Brush_unlatchPreferences(){
@@ -159,7 +159,7 @@ void Brush_Construct( EBrushType type ){
 	GlobalPreferenceSystem().registerPreference( "TexdefDefaultScale", FloatImportStringCaller( g_texdef_default_scale ), FloatExportStringCaller( g_texdef_default_scale ) );
 
 	GridStatus_getTextureLockEnabled = getTextureLockEnabled;
-	g_texture_lock_status_changed = FreeCaller<GridStatus_onTextureLockEnabledChanged>();
+	g_texture_lock_status_changed = FreeCaller<void(), GridStatus_onTextureLockEnabledChanged>();
 }
 
 void Brush_Destroy(){
@@ -188,8 +188,8 @@ void BrushFaceData_fromFace( const BrushFaceDataCallback& callback, Face& face )
 	faceData.value = face.getShader().m_flags.m_value;
 	callback( faceData );
 }
-typedef ConstReferenceCaller1<BrushFaceDataCallback, Face&, BrushFaceData_fromFace> BrushFaceDataFromFaceCaller;
-typedef Callback1<Face&> FaceCallback;
+typedef ConstReferenceCaller<BrushFaceDataCallback, void(Face&), BrushFaceData_fromFace> BrushFaceDataFromFaceCaller;
+typedef Callback<void(Face&)> FaceCallback;
 
 class Quake3BrushCreator : public BrushCreator
 {

radiant/commands.cpp
@@ -71,7 +71,7 @@ typedef std::map<CopiedString, Command> Commands;
 
 Commands g_commands;
 
-void GlobalCommands_insert( const char* name, const Callback& callback, const Accelerator& accelerator ){
+void GlobalCommands_insert( const char* name, const Callback<void()>& callback, const Accelerator& accelerator ){
 	bool added = g_commands.insert( Commands::value_type( name, Command( callback, GlobalShortcuts_insert( name, accelerator ) ) ) ).second;
 	ASSERT_MESSAGE( added, "command already registered: " << makeQuoted( name ) );
 }
@@ -87,7 +87,7 @@ typedef std::map<CopiedString, Toggle> Toggles;
 
 Toggles g_toggles;
 
-void GlobalToggles_insert( const char* name, const Callback& callback, const BoolExportCallback& exportCallback, const Accelerator& accelerator ){
+void GlobalToggles_insert( const char* name, const Callback<void()>& callback, const BoolExportCallback& exportCallback, const Accelerator& accelerator ){
 	bool added = g_toggles.insert( Toggles::value_type( name, Toggle( callback, GlobalShortcuts_insert( name, accelerator ), exportCallback ) ) ).second;
 	ASSERT_MESSAGE( added, "toggle already registered: " << makeQuoted( name ) );
 }
@@ -102,7 +102,7 @@ typedef std::map<CopiedString, KeyEvent> KeyEvents;
 
 KeyEvents g_keyEvents;
 
-void GlobalKeyEvents_insert( const char* name, const Accelerator& accelerator, const Callback& keyDown, const Callback& keyUp ){
+void GlobalKeyEvents_insert( const char* name, const Accelerator& accelerator, const Callback<void()>& keyDown, const Callback<void()>& keyUp ){
 	bool added = g_keyEvents.insert( KeyEvents::value_type( name, KeyEvent( GlobalShortcuts_insert( name, accelerator ), keyDown, keyUp ) ) ).second;
 	ASSERT_MESSAGE( added, "command already registered: " << makeQuoted( name ) );
 }

Garux/netradiant-custom

libs/gtkutil/accelerator.h
@@ -43,9 +43,9 @@ TextOutputStreamType& ostream_write( TextOutputStreamType& ostream, const QKeySe
 	return ostream;
 }
 
-void keydown_accelerators_add( QKeySequence accelerator, const Callback& callback );
+void keydown_accelerators_add( QKeySequence accelerator, const Callback<void()>& callback );
 void keydown_accelerators_remove( QKeySequence accelerator );
-void keyup_accelerators_add( QKeySequence accelerator, const Callback& callback );
+void keyup_accelerators_add( QKeySequence accelerator, const Callback<void()>& callback );
 void keyup_accelerators_remove( QKeySequence accelerator );
 
 void GlobalPressedKeys_releaseAll();
@@ -61,9 +61,9 @@ public:
 	QAction*& getAction() const {
 		return m_action;
 	}
-	Callback m_callback;
+	Callback<void()> m_callback;
 	const QKeySequence& m_accelerator;
-	Command( const Callback& callback, const QKeySequence& accelerator ) : m_callback( callback ), m_accelerator( accelerator ){
+	Command( const Callback<void()>& callback, const QKeySequence& accelerator ) : m_callback( callback ), m_accelerator( accelerator ){
 	}
 };
 
@@ -72,7 +72,7 @@ class Toggle
 public:
 	Command m_command;
 	BoolExportCallback m_exportCallback;
-	Toggle( const Callback& callback, const QKeySequence& accelerator, const BoolExportCallback& exportCallback ) : m_command( callback, accelerator ), m_exportCallback( exportCallback ){
+	Toggle( const Callback<void()>& callback, const QKeySequence& accelerator, const BoolExportCallback& exportCallback ) : m_command( callback, accelerator ), m_exportCallback( exportCallback ){
 	}
 };
 
@@ -80,8 +80,8 @@ class KeyEvent
 {
 public:
 	const QKeySequence& m_accelerator;
-	Callback m_keyDown;
-	Callback m_keyUp;
-	KeyEvent( const QKeySequence& accelerator, const Callback& keyDown, const Callback& keyUp ) : m_accelerator( accelerator ), m_keyDown( keyDown ), m_keyUp( keyUp ){
+	Callback<void()> m_keyDown;
+	Callback<void()> m_keyUp;
+	KeyEvent( const QKeySequence& accelerator, const Callback<void()>& keyDown, const Callback<void()>& keyUp ) : m_accelerator( accelerator ), m_keyDown( keyDown ), m_keyUp( keyUp ){
 	}
 };

radiant/brushmodule.cpp
@@ -77,12 +77,12 @@ const char* BrushType_getName( EBrushType type ){
 void Face_importSnapPlanes( bool value ){
 	Face::m_quantise = value ? quantiseInteger : quantiseFloating;
 }
-typedef FreeCaller1<bool, Face_importSnapPlanes> FaceImportSnapPlanesCaller;
+typedef FreeCaller<void(bool), Face_importSnapPlanes> FaceImportSnapPlanesCaller;
 
 void Face_exportSnapPlanes( const BoolImportCallback& importer ){
 	importer( Face::m_quantise == quantiseInteger );
 }
-typedef FreeCaller1<const BoolImportCallback&, Face_exportSnapPlanes> FaceExportSnapPlanesCaller;
+typedef FreeCaller<void(const BoolImportCallback&), Face_exportSnapPlanes> FaceExportSnapPlanesCaller;
 
 void Brush_constructPreferences( PreferencesPage& page ){
 	page.appendSpinner(
@@ -116,7 +116,7 @@ void Brush_constructPage( PreferenceGroup& group ){
 	Brush_constructPreferences( page );
 }
 void Brush_registerPreferencesPage(){
-	PreferencesDialog_addSettingsPage( FreeCaller1<PreferenceGroup&, Brush_constructPage>() );
+	PreferencesDialog_addSettingsPage( FreeCaller<void(PreferenceGroup&), Brush_constructPage>() );
 }
 
 void Brush_toggleFormat( EBrushType type ){
@@ -185,7 +185,7 @@ void Brush_Construct( EBrushType type ){
 
 	GridStatus_getTextureLockEnabled = getTextureLockEnabled;
 	GridStatus_getTexdefTypeIdLabel = getTexdefTypeIdLabel;
-	g_texture_lock_status_changed = FreeCaller<GridStatus_changed>();
+	g_texture_lock_status_changed = FreeCaller<void(), GridStatus_changed>();
 
 	Clipper_Construct();
 }
@@ -218,8 +218,8 @@ void BrushFaceData_fromFace( const BrushFaceDataCallback& callback, Face& face )
 	faceData.value = face.getShader().m_flags.m_value;
 	callback( faceData );
 }
-typedef ConstReferenceCaller1<BrushFaceDataCallback, Face&, BrushFaceData_fromFace> BrushFaceDataFromFaceCaller;
-typedef Callback1<Face&> FaceCallback;
+typedef ConstReferenceCaller<BrushFaceDataCallback, void(Face&), BrushFaceData_fromFace> BrushFaceDataFromFaceCaller;
+typedef Callback<void(Face&)> FaceCallback;
 
 class Quake3BrushCreator : public BrushCreator
 {

radiant/commands.cpp
@@ -68,7 +68,7 @@ typedef std::map<CopiedString, Command> Commands;
 
 Commands g_commands;
 
-void GlobalCommands_insert( const char* name, const Callback& callback, const QKeySequence& accelerator ){
+void GlobalCommands_insert( const char* name, const Callback<void()>& callback, const QKeySequence& accelerator ){
 	bool added = g_commands.insert( Commands::value_type( name, Command( callback, GlobalShortcuts_insert( name, accelerator ) ) ) ).second;
 	ASSERT_MESSAGE( added, "command already registered: " << makeQuoted( name ) );
 }
@@ -84,7 +84,7 @@ typedef std::map<CopiedString, Toggle> Toggles;
 
 Toggles g_toggles;
 
-void GlobalToggles_insert( const char* name, const Callback& callback, const BoolExportCallback& exportCallback, const QKeySequence& accelerator ){
+void GlobalToggles_insert( const char* name, const Callback<void()>& callback, const BoolExportCallback& exportCallback, const QKeySequence& accelerator ){
 	bool added = g_toggles.insert( Toggles::value_type( name, Toggle( callback, GlobalShortcuts_insert( name, accelerator ), exportCallback ) ) ).second;
 	ASSERT_MESSAGE( added, "toggle already registered: " << makeQuoted( name ) );
 }
@@ -99,7 +99,7 @@ typedef std::map<CopiedString, KeyEvent> KeyEvents;
 
 KeyEvents g_keyEvents;
 
-void GlobalKeyEvents_insert( const char* name, const Callback& keyDown, const Callback& keyUp, const QKeySequence& accelerator ){
+void GlobalKeyEvents_insert( const char* name, const Callback<void()>& keyDown, const Callback<void()>& keyUp, const QKeySequence& accelerator ){
 	bool added = g_keyEvents.insert( KeyEvents::value_type( name, KeyEvent( GlobalShortcuts_insert( name, accelerator ), keyDown, keyUp ) ) ).second;
 	ASSERT_MESSAGE( added, "command already registered: " << makeQuoted( name ) );
 }